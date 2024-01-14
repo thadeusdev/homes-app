45a14dd1544dac63da7e58a0c32bb62f3cfa2aaf
Thadeus <thadeusnyariki94@gmail.com>
2024-01-14 23:35:35 +0300

    01-create-home-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root', // to describe how Angular refers to the component in templates.
+  standalone: true, //to describe whether the component requires a NgModule
+  imports: [CommonModule, RouterOutlet, HomeComponent], //to describe the component's dependencies
+  templateUrl: './app.component.html', //to describe the component's HTML markup and layout
+  styleUrl: './app.component.css' //to list the URLs of the CSS files that the component uses in an array.
 })
 export class AppComponent {
-  title = 'Hello world!';
+  
 }
