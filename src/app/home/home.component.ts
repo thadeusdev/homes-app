19405021830aaa675cf4eb97127bc0579c0407ed
Thadeus <thadeusnyariki94@gmail.com>
2024-01-15 00:09:44 +0300

    creating-an-interface-housinglocation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,16 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
 
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
