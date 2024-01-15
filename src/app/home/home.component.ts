d0ddb5f74405236fb3fe0c398638a47a6114664f
Thadeus <thadeusnyariki94@gmail.com>
2024-01-15 14:19:08 +0300

    12-add-search-functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,8 +16,18 @@ export class HomeComponent {
 
   housingLocationList: Housinglocation[] = []
   housingService: HousingService = inject(HousingService)
+  filteredLocationList: Housinglocation[] = [];
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) => housingLocation?.city.toLowerCase().includes(text.toLowerCase()));
   }
 }
